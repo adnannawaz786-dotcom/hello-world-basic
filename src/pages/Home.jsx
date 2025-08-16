19c7e2e77e7bebb8d97dcb003297196cec780777
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-16 22:30:51 +0100

    Update src/pages/Home.jsx - Simple text content update from 'Hello World' to 'Hello Adnan' in relevant components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NeonText from '../components/NeonText';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <NeonText text="Hello Adnan" />
+    </div>
+  );
+};
+
+export default Home;
