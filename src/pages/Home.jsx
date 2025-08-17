37ff4964149c189b3838f91692c42c44d836f987
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-17 01:08:17 +0100

    Update src/pages/Home.jsx - Adding 'hello adnan' text to the Home.jsx component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,11 @@ import NeonText from '../components/NeonText';
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <NeonText text="HELLO ADNAN" className="text-6xl font-bold" />
+    <div className="min-h-screen bg-black flex flex-col items-center justify-center">
+      <NeonText>Welcome to Cyberpunk World</NeonText>
+      <div className="text-[#ff00ff] mt-4 text-2xl font-cyber animate-pulse">
+        hello adnan
+      </div>
     </div>
   );
 };
