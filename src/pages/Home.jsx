c8f009de8393290e1ca64c609a369a0a6748bea8
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-17 01:22:34 +0100

    Update src/pages/Home.jsx via IDE - individual file edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import NeonText from '../components/NeonText';
 const Home = () => {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center">
-      <NeonText text="Hello Samantha" className="text-6xl" />
+      <NeonText text="Hello Samantha1" className="text-6xl" />
     </div>
   );
 };
